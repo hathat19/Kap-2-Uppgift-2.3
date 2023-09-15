c49dab923ac4c2932b0f63e59bc0c3f960dba56d
humy06001 <humy06001@E-PW0234KS>
2023-09-15 10:13:53 +0200

    Add project files.

---
+++ Kap 2 Uppgift 2.3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Uppgift2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Frågar hur många dagar användaren ska låna bilen
+            Console.WriteLine("Hur många dagar vill du hyra bilen?");
+            int days = int.Parse(Console.ReadLine());
+
+            //Frågar hur många kilometer användaren ska köra
+            Console.WriteLine("Hur många kilometer tänker du köra med bilen?");
+            int kilometers = int.Parse(Console.ReadLine()); 
+
+            //Räknar ut summan
+            int sum = 300 + kilometers + 500*(days-1);
+
+            //Skriver ut summan
+            Console.WriteLine($"Det kostar totalt {sum}kr att hyra bilen.");
+        }
+    }
+}
